8ed794cd14dee4083bab5aeede60b69ca1b39b5f
Adrian Grimm <usmcamgrimm@gmail.com>
2019-10-01 06:48:40 -0400

    start adding nav bar

---
+++ js/navScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myNav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
