03aa8fe5a08869e1be034a96b0fae841a2c3f745
Adrian Grimm <usmcamgrimm@gmail.com>
2019-10-08 20:16:08 -0400

    Work with images
    ....and un-fuck the media queries

--- js/navScript.js
+++ js/navScript.js
@@ -1,5 +1,5 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById('navMenu');
+let navBarToggle = document.getElementById('navToggle');
 
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
