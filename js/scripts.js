a79d708cf2cd0f82854b0d706bd2fdaefc51a6af
Adrian Grimm <usmcamgrimm@gmail.com>
2019-11-28 11:03:29 -0500

    Minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,10 @@ const myDev = setInterval(devRandom, 2000);
 
 function devRandom() {
     document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
+};
+
+const myMobDev = setInterval(mobRandom, 2000);
+
+function mobRandom() {
+    document.getElementById("mobDev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
 }
