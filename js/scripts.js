41cc17ac07fe76740caed1c6bc4135ab0afd5bdb
Adrian Grimm <usmcamgrimm@gmail.com>
2020-01-17 08:22:12 -0500

    Update random interval

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,13 @@ navBarToggle.addEventListener('click', function () {
 // Script to change title on main screen
 let devArray = ['Front End Developer', 'Developer', 'Web Developer', 'Programmer'];
 
-const myDev = setInterval(devRandom, 2000);
+const myDev = setInterval(devRandom, 1500);
 
 function devRandom() {
     document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
 };
 
-const myMobDev = setInterval(mobRandom, 2000);
+const myMobDev = setInterval(mobRandom, 1800);
 
 function mobRandom() {
     document.getElementById("mobDev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
