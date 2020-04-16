7c273c8f1aa00cd0fdc95003fac923c92c2eda7e
Adrian Grimm <usmcamgrimm@gmail.com>
2020-04-16 10:22:58 -0400

    Slight adjustment to random timer

--- js/randomDev.js
+++ js/randomDev.js
@@ -1,7 +1,7 @@
 // Script to change title on main screen
-let devArray = ['Front-End Developer', 'Developer', 'Web Developer', 'Programmer', 'Marine Corps Veteran', 'Husband', 'Father'];
+let devArray = ['Front-End Developer', 'Developer', 'Web Developer', 'Coder', 'Programmer', 'Marine Corps Veteran', 'Husband', 'Father'];
 
-const myDev = setInterval(devRandom, 1200);
+const myDev = setInterval(devRandom, 1000);
 
 function devRandom() {
     document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
