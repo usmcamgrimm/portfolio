d0fd064a968f847a3ce39327af4962fd5d445a4d
Adrian Grimm <usmcamgrimm@gmail.com>
2020-07-23 11:38:50 -0400

    Add typed.js

--- js/randomDev.js
+++ js/randomDev.js
@@ -1,8 +1,23 @@
-// Script to change title on main screen
-let devArray = ['Front-End Developer', 'Developer', 'Web Developer', 'Coder', 'Programmer', 'Marine Corps Veteran', 'Husband', 'Father'];
+// // Script to change title on main screen
+// let devArray = ['Front-End Developer', 'Developer', 'Web Developer', 'Coder', 'Programmer', 'Marine Corps Veteran', 'Husband', 'Father'];
 
-const myDev = setInterval(devRandom, 1000);
+// const myDev = setInterval(devRandom, 1000);
 
-function devRandom() {
-    document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
-};
+// function devRandom() {
+//     document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
+// };
+
+let typed3 = new Typed('#typed3', {
+    strings: [
+        'I am a Front-End Developer',
+        'I am a Developer',
+        'I am a Web Developer',
+        'I am a JavaScript Developer',
+        'I am a Marine Corps Veteran'
+    ],
+    typeSpeed: 70,
+    backSpeed: 45,
+    shuffle: true,
+    smartBackspace: true,
+    loop: true
+});
