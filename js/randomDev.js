a4217bafd162d8752f171277dceac4368eac862c
Adrian Grimm <usmcamgrimm@gmail.com>
2020-07-24 06:00:42 -0400

    Clean up the old random JS function

--- js/randomDev.js
+++ js/randomDev.js
@@ -1,12 +1,3 @@
-// // Script to change title on main screen
-// let devArray = ['Front-End Developer', 'Developer', 'Web Developer', 'Coder', 'Programmer', 'Marine Corps Veteran', 'Husband', 'Father'];
-
-// const myDev = setInterval(devRandom, 1000);
-
-// function devRandom() {
-//     document.getElementById("dev").innerHTML = devArray[Math.floor(Math.random() * devArray.length)];
-// };
-
 let typed3 = new Typed('#typed3', {
     strings: [
         'I am a Front-End Developer',
