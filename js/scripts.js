3ef8ab938445d01374fd08c96d28bc7c41156cf5
Adrian Grimm <usmcamgrimm@gmail.com>
2021-03-20 11:04:19 -0400

    Don't change tab title when leaving this tab

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,3 @@ function showNav() {
         x.style.display = "block";
     }
 }
-
-// Change tab title
-window.onblur = function () {
-    document.title = 'Come back soon!';
-}
-window.onfocus = function () {
-    document.title = 'Adrian Grimm';
-}
