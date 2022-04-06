58c1aea33e10bcfc93d8d4feb7704d0cddb4665e
Adrian Grimm <usmcamgrimm@gmail.com>
2022-04-06 11:56:48 -0400

    Starting from scratch

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Mobile Navbar toggle script
-function showNav() {
-    let x = document.getElementById("mobLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
